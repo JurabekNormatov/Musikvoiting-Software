1412c1d26e88d94fba1b91dfa05760d9dad7b4db
Roman <romanfen1997@gmail.com>
2024-12-19 09:03:17 +0100

    Remove unnecessary buttons from Gastgeber page

--- MusikVoting/src/views/Gastgeber.vue
+++ MusikVoting/src/views/Gastgeber.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="flex-container">
+  <div class="flex-container width-20">
     <div> 
     <input type="text" placeholder="Playlist Name" class="input-text"/>
     </div>
     <div>
       <button @click="handleButtonClick('Add')" class="btn">Add</button>
-      <button @click="handleButtonClick('Login')" class="btn">Login</button>
       <button @click="handleButtonClick('Delete')" class="btn">Delete</button>
-      <button @click="handleButtonClick('New Playlist')" class="btn">New Playlist</button>
-      <button @click="handleButtonClick('Filter')" class="btn">Filter</button>
     </div>
   </div>
 </template>
