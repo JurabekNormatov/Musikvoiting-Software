d08936790e8757fe1efbe2dd481cbd91542d664a
Nevydaylo <nevydaylo95@gmail.com>
2025-02-11 12:59:09 +0100

    update database

--- MusikVoting/src/database/MusikVotingDB.sql
+++ MusikVoting/src/database/MusikVotingDB.sql
@@ -1,14 +1,13 @@
-/*!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19  Distrib 10.11.8-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 9.0.1, for macos13.7 (x86_64)
 --
 -- Host: localhost    Database: MusikVotingDB
 -- ------------------------------------------------------
--- Server version	10.11.8-MariaDB-0ubuntu0.24.04.1
+-- Server version	9.0.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -22,13 +21,13 @@
 
 DROP TABLE IF EXISTS `T_Gast`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Gast` (
-  `gast_id` int(11) NOT NULL,
+  `gast_id` int NOT NULL AUTO_INCREMENT,
   `vname` varchar(50) DEFAULT NULL,
   `nname` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`gast_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,10 +36,7 @@ CREATE TABLE `T_Gast` (
 
 LOCK TABLES `T_Gast` WRITE;
 /*!40000 ALTER TABLE `T_Gast` DISABLE KEYS */;
-INSERT INTO `T_Gast` VALUES
-(1,'Jaime','Lennister'),
-(2,'Cersei','Lennister'),
-(3,'Daenerys','Targaryen');
+INSERT INTO `T_Gast` VALUES (1,'Jaime','Lennister'),(2,'Cersei','Lennister'),(3,'Daenerys','Targaryen'),(4,'Admin','1234'),(5,'roma','fen'),(6,'roma ','fen'),(7,'Max','Muster'),(8,'Dima','Nevydaylo'),(9,'Jurabek','Nurmagomedov'),(10,'test','test'),(11,'max','mustermann'),(12,'123','Nevydaylo'),(13,'Dimadi','dddd'),(14,'43432','grgrgrrg'),(15,'scjscjsjsdj','sjjdsnfenhfce'),(16,'fvmkfnvjfnb','jdcnwjncwjv'),(17,'ffmemfem','dfdnfjdf'),(18,'565656','edfdefe'),(19,'5959595959','fknveofpnvlafkjndvlkanejv'),(20,'brgbrbrb','dcdcd'),(21,'eferfe','rfrrfr'),(22,'Muster','Mister'),(23,'Maxxx','Mustermann'),(24,'546456','gfwgfegweg'),(25,'3457437','35747437');
 /*!40000 ALTER TABLE `T_Gast` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,13 +46,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `T_Gastgeber`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Gastgeber` (
-  `gastgeber_id` int(11) NOT NULL,
+  `gastgeber_id` int NOT NULL AUTO_INCREMENT,
   `vname` varchar(50) DEFAULT NULL,
   `nname` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`gastgeber_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,8 +61,7 @@ CREATE TABLE `T_Gastgeber` (
 
 LOCK TABLES `T_Gastgeber` WRITE;
 /*!40000 ALTER TABLE `T_Gastgeber` DISABLE KEYS */;
-INSERT INTO `T_Gastgeber` VALUES
-(1,'Tyrion','Lennister');
+INSERT INTO `T_Gastgeber` VALUES (1,'Tyrion','Lennister');
 /*!40000 ALTER TABLE `T_Gastgeber` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,15 +71,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `T_Musikwunsch`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Musikwunsch` (
-  `song_id` int(11) NOT NULL,
+  `song_id` int NOT NULL AUTO_INCREMENT,
   `titel` varchar(100) DEFAULT NULL,
   `genre` varchar(50) DEFAULT NULL,
   `bandname` varchar(100) DEFAULT NULL,
-  `votes_count` int(11) DEFAULT 0,
+  `votes_count` int DEFAULT '0',
   PRIMARY KEY (`song_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,13 +88,7 @@ CREATE TABLE `T_Musikwunsch` (
 
 LOCK TABLES `T_Musikwunsch` WRITE;
 /*!40000 ALTER TABLE `T_Musikwunsch` DISABLE KEYS */;
-INSERT INTO `T_Musikwunsch` VALUES
-(1,'Raining Blood','Thrash Metal','Slayer',3),
-(2,'Nothing Else Matters','Ballade','Metallica',2),
-(3,'Atemlos durch die Nacht','Schlager','Helene Fischer',1),
-(4,'Die Wahrheit über die Liebe','Schlager','Helene Fischer',0),
-(5,'Master of Puppets','Thrash Metal','Metallica',2),
-(6,'Hallelujah','Pop','Leonard Cohen',2);
+INSERT INTO `T_Musikwunsch` VALUES (1,'Raining Blood','Thrash Metal','Slayer',58),(2,'Nothing Else Matters','Ballade','Metallica',66);
 /*!40000 ALTER TABLE `T_Musikwunsch` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,15 +98,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `T_Playlist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Playlist` (
-  `playlist_id` int(11) NOT NULL,
-  `f_gastgeber_id` int(11) DEFAULT NULL,
+  `playlist_id` int NOT NULL AUTO_INCREMENT,
+  `f_gastgeber_id` int DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`playlist_id`),
   KEY `f_gastgeber_id` (`f_gastgeber_id`),
-  CONSTRAINT `T_Playlist_ibfk_1` FOREIGN KEY (`f_gastgeber_id`) REFERENCES `T_Gastgeber` (`gastgeber_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `t_playlist_ibfk_1` FOREIGN KEY (`f_gastgeber_id`) REFERENCES `T_Gastgeber` (`gastgeber_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,8 +115,7 @@ CREATE TABLE `T_Playlist` (
 
 LOCK TABLES `T_Playlist` WRITE;
 /*!40000 ALTER TABLE `T_Playlist` DISABLE KEYS */;
-INSERT INTO `T_Playlist` VALUES
-(1,1,'Party-Time');
+INSERT INTO `T_Playlist` VALUES (1,1,'Party-Time'),(2,1,'new playlist'),(3,1,'4444'),(4,1,'ecewcwecw'),(5,1,'TestPlaylist'),(6,1,'sasasasasas'),(7,1,'Dima'),(8,1,'Test'),(9,1,'Test Playlist'),(10,1,'34343434'),(11,1,'Dimas Playlist'),(12,1,'Test3');
 /*!40000 ALTER TABLE `T_Playlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,16 +125,17 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `T_Playlist_Song`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Playlist_Song` (
-  `playlist_id` int(11) NOT NULL,
-  `song_id` int(11) NOT NULL,
-  `position` int(11) DEFAULT NULL,
+  `playlist_id` int NOT NULL,
+  `song_id` int NOT NULL,
+  `position` int DEFAULT NULL,
   PRIMARY KEY (`playlist_id`,`song_id`),
-  KEY `song_id` (`song_id`),
-  CONSTRAINT `T_Playlist_Song_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `T_Playlist` (`playlist_id`),
-  CONSTRAINT `T_Playlist_Song_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `T_Musikwunsch` (`song_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `playlist_song_fk_song` (`song_id`),
+  CONSTRAINT `playlist_song_fk_song` FOREIGN KEY (`song_id`) REFERENCES `T_Musikwunsch` (`song_id`) ON DELETE CASCADE,
+  CONSTRAINT `t_playlist_song_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `T_Playlist` (`playlist_id`),
+  CONSTRAINT `t_playlist_song_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `T_Musikwunsch` (`song_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,13 +144,7 @@ CREATE TABLE `T_Playlist_Song` (
 
 LOCK TABLES `T_Playlist_Song` WRITE;
 /*!40000 ALTER TABLE `T_Playlist_Song` DISABLE KEYS */;
-INSERT INTO `T_Playlist_Song` VALUES
-(1,1,1),
-(1,2,2),
-(1,3,5),
-(1,4,NULL),
-(1,5,3),
-(1,6,4);
+INSERT INTO `T_Playlist_Song` VALUES (1,1,1),(1,2,2);
 /*!40000 ALTER TABLE `T_Playlist_Song` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -171,19 +154,20 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `T_Vote`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_Vote` (
-  `vote_id` int(11) NOT NULL,
-  `f_gast_id` int(11) DEFAULT NULL,
-  `f_song_id` int(11) DEFAULT NULL,
+  `vote_id` int NOT NULL AUTO_INCREMENT,
+  `f_gast_id` int DEFAULT NULL,
+  `f_song_id` int DEFAULT NULL,
   `datum` date DEFAULT NULL,
   `zeit` time DEFAULT NULL,
   PRIMARY KEY (`vote_id`),
   KEY `f_gast_id` (`f_gast_id`),
-  KEY `f_song_id` (`f_song_id`),
-  CONSTRAINT `T_Vote_ibfk_1` FOREIGN KEY (`f_gast_id`) REFERENCES `T_Gast` (`gast_id`),
-  CONSTRAINT `T_Vote_ibfk_2` FOREIGN KEY (`f_song_id`) REFERENCES `T_Musikwunsch` (`song_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `t_vote_ibfk_2` (`f_song_id`),
+  CONSTRAINT `t_vote_ibfk_1` FOREIGN KEY (`f_gast_id`) REFERENCES `T_Gast` (`gast_id`),
+  CONSTRAINT `t_vote_ibfk_2` FOREIGN KEY (`f_song_id`) REFERENCES `T_Musikwunsch` (`song_id`) ON DELETE CASCADE,
+  CONSTRAINT `vote_fk_song` FOREIGN KEY (`f_song_id`) REFERENCES `T_Musikwunsch` (`song_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -192,17 +176,7 @@ CREATE TABLE `T_Vote` (
 
 LOCK TABLES `T_Vote` WRITE;
 /*!40000 ALTER TABLE `T_Vote` DISABLE KEYS */;
-INSERT INTO `T_Vote` VALUES
-(1,1,1,'2023-11-24','12:00:00'),
-(2,2,2,'2023-11-24','12:05:00'),
-(3,3,3,'2023-11-24','12:10:00'),
-(4,1,5,'2023-11-24','12:15:00'),
-(5,2,6,'2023-11-24','12:20:00'),
-(6,3,1,'2023-11-24','12:25:00'),
-(7,1,2,'2023-11-24','12:30:00'),
-(8,2,6,'2023-11-24','12:35:00'),
-(9,3,5,'2023-11-24','12:40:00'),
-(10,1,1,'2023-11-24','12:45:00');
+INSERT INTO `T_Vote` VALUES (1,1,1,'2023-11-24','12:00:00'),(2,2,2,'2023-11-24','12:05:00'),(6,3,1,'2023-11-24','12:25:00'),(7,1,2,'2023-11-24','12:30:00'),(10,1,1,'2023-11-24','12:45:00');
 /*!40000 ALTER TABLE `T_Vote` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -215,4 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-18 13:26:15
+-- Dump completed on 2025-02-11 11:29:56
